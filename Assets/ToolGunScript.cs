6dbdb75249e611abc2744bf43404f27210b617a7
Forelone <ForeTheDev@proton.me>
2026-02-17 00:04:37 +0300

    Uhhh yes.

---
+++ Assets/ToolGunScript.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ToolGunScript : MonoBehaviour
+{
+    /*Bu nedir?
+
+        Bu script bütün aletleri tek bir alette toplayan bir scripttir.
+
+        Nasıl çalışır?
+
+            Oyuncu menüye girmek için sağ tıklar
+            Oyuncu menü'den almak istediği alete mouse tekerleği ile gidip sol tıklar
+            alet'in çalışma şekline göre ilerler.
+        
+        Kaç tane alet olacak?
+            
+            ekleyebildiğimiz kadar!
+
+        Bununla nasıl başa çıkacaksın?
+
+            I dunno?
+
+    */
+}
