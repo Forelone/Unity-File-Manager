9b550757dccbaadd527ebe2ddffe7f954ee20c64
Forelone <ForeTheDev@proton.me>
2026-02-16 21:53:43 +0300

    Image file fix

--- Assets/ImageHandler.cs
+++ Assets/ImageHandler.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class ImageHandler : MonoBehaviour
 {
+    PathProtocol PP;
+    public void SetPeePee(PathProtocol P) => PP = P;
+
     public void Toggle()
     {
         enabled = !enabled;
@@ -10,25 +13,31 @@ public class ImageHandler : MonoBehaviour
 
     void OnEnable()
     {
+        PP = transform.parent.GetComponentInParent<PathProtocol>();
+        if (PP == null) Debug.LogError("Can't find my peenie :/", gameObject);
         //First we get full path
-        PathProtocol PP = GetComponentInParent<PathProtocol>();
-        string Name = transform.name;
+        string Name = gameObject.transform.name;
         string FullPath = PP.GetPath() + Name;
         print(FullPath);
         //Then we check if we can convert it
         byte[] FileData = File.ReadAllBytes(FullPath);
         Texture2D Texture = new Texture2D(2, 2);
-        Texture.LoadImage(FileData);
+        bool SuccessfullyLoadedData = Texture.LoadImage(FileData);
         //Then we create material with this image (or not)
-        transform.GetChild(0).GetComponent<Renderer>().material.mainTexture = Texture;
+        if (SuccessfullyLoadedData)
+        {
+        transform.GetComponentInChildren<Renderer>().material.mainTexture = Texture;
         int W = Mathf.Clamp(Texture.width / 100, 1, 4), H = Mathf.Clamp(Texture.height / 100, 1, 4);
         //???
         transform.localScale = new Vector3(W, 0.03f, H);
         //Profit!!!
+        }
+        else
+        Debug.LogError("Something went wrong while trying to load this Image.");
     }
 
     void OnDisable()
     {
-        Destroy(transform.GetChild(0).GetComponent<Renderer>().material.mainTexture);
+        Destroy(transform.GetComponentInChildren<Renderer>().material.mainTexture);
     }
 }
